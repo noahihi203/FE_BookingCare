f0c1821a8f361aedfb0f1766e7be244e38de7a22
noahihi203 <da02470247@example.com>
2024-10-15 01:40:22 +0700

    ✅ video 50

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -1,31 +1,45 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import HomeHeader from './HomeHeader';
-import Specialty from './Section/Specialty';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import HomeHeader from "./HomeHeader";
+import Specialty from "./Section/Specialty";
+import MedicalFacility from "./Section/MedicalFacility";
+import OutStandingDoctor from "./Section/OutStandingDoctor";
+import HandBook from "./Section/HandBook";
+import "./HomePage.scss";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { set } from "lodash";
+
 class HomePage extends Component {
-
-    render() {
-
-        return (
-            <div>
-                <HomeHeader/>
-                <Specialty/>
-                <div style={{height: '300px'}}></div>
-            </div>
-        );
-    }
-
+  render() {
+    let settings = {
+      dots: false,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+    };
+    return (
+      <div>
+        <HomeHeader />
+        <Specialty settings={settings} />
+        <MedicalFacility settings={settings} />
+        <OutStandingDoctor settings={settings} />
+        <HandBook settings={settings}/>
+        <div style={{ height: "300px" }}></div>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = state => {
-    return {
-        isLoggedIn: state.user.isLoggedIn
-    };
+const mapStateToProps = (state) => {
+  return {
+    isLoggedIn: state.user.isLoggedIn,
+  };
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-    };
+const mapDispatchToProps = (dispatch) => {
+  return {};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
