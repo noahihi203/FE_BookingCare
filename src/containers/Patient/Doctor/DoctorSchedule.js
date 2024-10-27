7500efdb1dc16ad2a14ad8ea91441ea7fb12be8b
noahihi203 <da02470247@gmail.com>
2024-10-27 23:56:10 +0700

    add content-left, content-right

--- src/containers/Patient/Doctor/DoctorSchedule.js
+++ src/containers/Patient/Doctor/DoctorSchedule.js
@@ -1,84 +1,17 @@
 import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
-import HomeHeader from "../../HomePage/HomeHeader";
 import "./DoctorSchedule.scss";
-import { getDetailInfoDoctorSchedule } from "../../../services/userService";
-import { LANGUAGES } from "../../../utils";
 class DoctorSchedule extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      detailDoctor: {},
-    };
+    this.state = {};
   }
-  async componentDidMount() {
-    if (
-      this.props.match &&
-      this.props.match.params &&
-      this.props.match.params.id
-    ) {
-      let id = this.props.match.params.id;
-      let res = await getDetailInfoDoctorSchedule(id);
-      if (res && res.errCode === 0) {
-        this.setState({
-          detailDoctor: res.data,
-        });
-      }
-    }
-  }
+  async componentDidMount() {}
 
   componentDidUpdate(prevProps, prevState, snapshot) {}
 
   render() {
-    //console.log("noah check: ", this.state.detailDoctor);
-    let { language } = this.props;
-    let { detailDoctor } = this.state;
-    let nameVi = "",
-      nameEn = "";
-    if (detailDoctor && detailDoctor.positionData) {
-      nameVi = `${detailDoctor.positionData.valueVi}, ${detailDoctor.lastName} ${detailDoctor.firstName}`;
-      nameEn = `${detailDoctor.positionData.valueEn}, ${detailDoctor.firstName} ${detailDoctor.lastName}`;
-    }
-    return (
-      <Fragment>
-        <HomeHeader isShowBanner={false} />
-        <div className="doctor-detail-container">
-          <div className="intro-doctor">
-            <div
-              className="content-left"
-              style={{
-                backgroundImage: `url(${
-                  detailDoctor && detailDoctor.image ? detailDoctor.image : ""
-                })`,
-              }}
-            ></div>
-            <div className="content-right">
-              <div className="up">
-                {language === LANGUAGES.VI ? nameVi : nameEn}
-              </div>
-              <div className="down">
-                {detailDoctor.Markdown && detailDoctor.Markdown.description && (
-                  <span>{detailDoctor.Markdown.description}</span>
-                )}
-              </div>
-            </div>
-          </div>
-          <div className="schedule-doctor"></div>
-          <div className="detail-info-doctor">
-            {detailDoctor &&
-              detailDoctor.Markdown &&
-              detailDoctor.Markdown.contentHTML && (
-                <div
-                  dangerouslySetInnerHTML={{
-                    __html: detailDoctor.Markdown.contentHTML,
-                  }}
-                ></div>
-              )}
-          </div>
-          <div className="comment-doctor"></div>
-        </div>
-      </Fragment>
-    );
+    return <div>doctor schedule</div>;
   }
 }
 
